a7b0a33077afba1f0df12141a167114e83a13721
Cai Parry-Jones <97813242+caitpj@users.noreply.github.com>
2022-10-22 14:34:21 +0100

    Format bug fix

--- clean_playstation_data.sql
+++ clean_playstation_data.sql
@@ -1,241 +1,41 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2639
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Bold;\f1\fnil\fcharset0 Menlo-Regular;}
-{\colortbl;\red255\green255\blue255;\red115\green158\blue202;\red170\green170\blue170;\red158\green158\blue158;
-\red193\green170\blue108;\red202\green197\blue128;\red192\green192\blue192;}
-{\*\expandedcolortbl;;\csgenericrgb\c45098\c61961\c79216;\csgenericrgb\c66667\c66667\c66667;\csgenericrgb\c61961\c61961\c61961;
-\csgenericrgb\c75686\c66667\c42353;\csgenericrgb\c79216\c77255\c50196;\csgenericrgb\c75294\c75294\c75294;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
+with base as (
+select 
+	name as product_name,
+	platforms as platform,
+	case when lower(original_price) = 'unavailable' or original_price is null then False else True end as is_available,
+	cast(CASE 
+		when lower(original_price) in ('free') or lower(original_price) like '%trial%' then 0
+		when lower(original_price) = 'unavailable' or original_price is null then null
+		when original_price like '£%' then REPLACE(original_price, '£', '')
+		else original_price
+	END as DECIMAL(10,2)) as original_price_gbp,
+	cast(CASE 
+		when lower(discount_price) in ('free') or lower(discount_price) like '%trial%' then 0
+		when lower(discount_price) = 'unavailable' or discount_price is null then null
+		when discount_price like '£%' then REPLACE(discount_price, '£', '')
+		else discount_price
+	END as DECIMAL(10,2)) as discounted_price_gbp,
+	discount,
+	product_type,
+	sale_name,
+	rank_in_sale,
+	`date` as date_recorded
+FROM my_db.playstation_store_games
+)
 
-\f0\b\fs24 \cf2 with
-\f1\b0 \cf3  \cf4 base\cf3  
-\f0\b \cf2 as
-\f1\b0 \cf3  (\cf0 \
-
-\f0\b \cf2 select
-\f1\b0 \cf3  \cf0 \
-\pard\pardeftab720\partightenfactor0
-\cf3 	\cf4 name\cf3  
-\f0\b \cf2 as
-\f1\b0 \cf3  \cf4 product_name\cf3 ,\cf0 \
-\cf3 	\cf4 platforms\cf3  
-\f0\b \cf2 as
-\f1\b0 \cf3  \cf4 platform\cf3 ,\cf0 \
-\cf3 	
-\f0\b \cf2 case
-\f1\b0 \cf3  
-\f0\b \cf2 when
-\f1\b0 \cf3  
-\f0\b \cf5 lower
-\f1\b0 \cf3 (\cf4 original_price\cf3 ) = \cf6 'unavailable'\cf3  
-\f0\b \cf2 or
-\f1\b0 \cf3  \cf4 original_price\cf3  
-\f0\b \cf2 is
-\f1\b0 \cf3  
-\f0\b \cf2 null
-\f1\b0 \cf3  
-\f0\b \cf2 then
-\f1\b0 \cf3  
-\f0\b \cf2 False
-\f1\b0 \cf3  
-\f0\b \cf2 else
-\f1\b0 \cf3  
-\f0\b \cf2 True
-\f1\b0 \cf3  
-\f0\b \cf2 end
-\f1\b0 \cf3  
-\f0\b \cf2 as
-\f1\b0 \cf3  \cf4 is_available\cf3 ,\cf0 \
-\cf3 	
-\f0\b \cf5 cast
-\f1\b0 \cf3 (
-\f0\b \cf2 CASE
-\f1\b0 \cf3  \cf0 \
-\cf3 		
-\f0\b \cf2 when
-\f1\b0 \cf3  
-\f0\b \cf5 lower
-\f1\b0 \cf3 (\cf4 original_price\cf3 ) 
-\f0\b \cf2 in
-\f1\b0 \cf3  (\cf6 'free'\cf3 ) 
-\f0\b \cf2 or
-\f1\b0 \cf3  
-\f0\b \cf5 lower
-\f1\b0 \cf3 (\cf4 original_price\cf3 ) 
-\f0\b \cf2 like
-\f1\b0 \cf3  \cf6 '%trial%'\cf3  
-\f0\b \cf2 then
-\f1\b0 \cf3  \cf7 0\cf0 \
-\cf3 		
-\f0\b \cf2 when
-\f1\b0 \cf3  
-\f0\b \cf5 lower
-\f1\b0 \cf3 (\cf4 original_price\cf3 ) = \cf6 'unavailable'\cf3  
-\f0\b \cf2 or
-\f1\b0 \cf3  \cf4 original_price\cf3  
-\f0\b \cf2 is
-\f1\b0 \cf3  
-\f0\b \cf2 null
-\f1\b0 \cf3  
-\f0\b \cf2 then
-\f1\b0 \cf3  
-\f0\b \cf2 null
-\f1\b0 \cf0 \
-\cf3 		
-\f0\b \cf2 when
-\f1\b0 \cf3  \cf4 original_price\cf3  
-\f0\b \cf2 like
-\f1\b0 \cf3  \cf6 '\'a3%'\cf3  
-\f0\b \cf2 then
-\f1\b0 \cf3  
-\f0\b \cf5 REPLACE
-\f1\b0 \cf3 (\cf4 original_price\cf3 , \cf6 '\'a3'\cf3 , \cf6 ''\cf3 )\cf0 \
-\cf3 		
-\f0\b \cf2 else
-\f1\b0 \cf3  \cf4 original_price\cf0 \
-\cf3 	
-\f0\b \cf2 END
-\f1\b0 \cf3  
-\f0\b \cf2 as
-\f1\b0 \cf3  
-\f0\b \cf5 DECIMAL
-\f1\b0 \cf3 (\cf7 10\cf3 ,\cf7 2\cf3 )) 
-\f0\b \cf2 as
-\f1\b0 \cf3  \cf4 original_price_gbp\cf3 ,\cf0 \
-\cf3 	
-\f0\b \cf5 cast
-\f1\b0 \cf3 (
-\f0\b \cf2 CASE
-\f1\b0 \cf3  \cf0 \
-\cf3 		
-\f0\b \cf2 when
-\f1\b0 \cf3  
-\f0\b \cf5 lower
-\f1\b0 \cf3 (\cf4 discount_price\cf3 ) 
-\f0\b \cf2 in
-\f1\b0 \cf3  (\cf6 'free'\cf3 ) 
-\f0\b \cf2 or
-\f1\b0 \cf3  
-\f0\b \cf5 lower
-\f1\b0 \cf3 (\cf4 discount_price\cf3 ) 
-\f0\b \cf2 like
-\f1\b0 \cf3  \cf6 '%trial%'\cf3  
-\f0\b \cf2 then
-\f1\b0 \cf3  \cf7 0\cf0 \
-\cf3 		
-\f0\b \cf2 when
-\f1\b0 \cf3  
-\f0\b \cf5 lower
-\f1\b0 \cf3 (\cf4 discount_price\cf3 ) = \cf6 'unavailable'\cf3  
-\f0\b \cf2 or
-\f1\b0 \cf3  \cf4 discount_price\cf3  
-\f0\b \cf2 is
-\f1\b0 \cf3  
-\f0\b \cf2 null
-\f1\b0 \cf3  
-\f0\b \cf2 then
-\f1\b0 \cf3  
-\f0\b \cf2 null
-\f1\b0 \cf0 \
-\cf3 		
-\f0\b \cf2 when
-\f1\b0 \cf3  \cf4 discount_price\cf3  
-\f0\b \cf2 like
-\f1\b0 \cf3  \cf6 '\'a3%'\cf3  
-\f0\b \cf2 then
-\f1\b0 \cf3  
-\f0\b \cf5 REPLACE
-\f1\b0 \cf3 (\cf4 discount_price\cf3 , \cf6 '\'a3'\cf3 , \cf6 ''\cf3 )\cf0 \
-\cf3 		
-\f0\b \cf2 else
-\f1\b0 \cf3  \cf4 discount_price\cf0 \
-\cf3 	
-\f0\b \cf2 END
-\f1\b0 \cf3  
-\f0\b \cf2 as
-\f1\b0 \cf3  
-\f0\b \cf5 DECIMAL
-\f1\b0 \cf3 (\cf7 10\cf3 ,\cf7 2\cf3 )) 
-\f0\b \cf2 as
-\f1\b0 \cf3  \cf4 discounted_price_gbp\cf3 ,\cf0 \
-\cf3 	\cf4 discount\cf3 ,\cf0 \
-\cf3 	\cf4 product_type\cf3 ,\cf0 \
-\cf3 	\cf4 sale_name\cf3 ,\cf0 \
-\cf3 	\cf4 rank_in_sale\cf3 ,\cf0 \
-\cf3 	\cf5 `date`\cf3  
-\f0\b \cf2 as
-\f1\b0 \cf3  \cf4 date_recorded\cf0 \
-\pard\pardeftab720\partightenfactor0
-
-\f0\b \cf2 FROM
-\f1\b0 \cf3  \cf4 my_db\cf3 .\cf4 playstation_store_games\cf0 \
-\pard\pardeftab720\partightenfactor0
-\cf3 )\cf0 \
-\
-\pard\pardeftab720\partightenfactor0
-
-\f0\b \cf2 select
-\f1\b0 \cf0 \
-\pard\pardeftab720\partightenfactor0
-\cf3 	\cf4 product_name\cf3 ,\cf0 \
-\cf3 	\cf4 platform\cf3 ,\cf0 \
-\cf3 	\cf4 is_available\cf3 ,\cf0 \
-\cf3 	\cf4 original_price_gbp\cf3 ,\cf0 \
-\cf3 	
-\f0\b \cf2 case
-\f1\b0 \cf3  
-\f0\b \cf2 when
-\f1\b0 \cf3  \cf4 discounted_price_gbp\cf3  != \cf4 original_price_gbp\cf3  
-\f0\b \cf2 then
-\f1\b0 \cf3  
-\f0\b \cf2 True
-\f1\b0 \cf3  
-\f0\b \cf2 else
-\f1\b0 \cf3  
-\f0\b \cf2 False
-\f1\b0 \cf3  
-\f0\b \cf2 end
-\f1\b0 \cf3  
-\f0\b \cf2 as
-\f1\b0 \cf3  \cf4 is_discounted\cf3 ,\cf0 \
-\cf3 	
-\f0\b \cf2 case
-\f1\b0 \cf3  
-\f0\b \cf2 when
-\f1\b0 \cf3  \cf4 discounted_price_gbp\cf3  != \cf4 original_price_gbp\cf3  
-\f0\b \cf2 then
-\f1\b0 \cf3  \cf4 discounted_price_gbp\cf3  
-\f0\b \cf2 else
-\f1\b0 \cf3  
-\f0\b \cf2 null
-\f1\b0 \cf3  
-\f0\b \cf2 end
-\f1\b0 \cf3  
-\f0\b \cf2 as
-\f1\b0 \cf3  \cf4 discounted_price_gbp\cf3 ,\cf0 \
-\cf3 	\cf4 discount\cf3  
-\f0\b \cf2 as
-\f1\b0 \cf3  \cf4 display_discount\cf3 ,\cf0 \
-\cf3 	
-\f0\b \cf2 case
-\f1\b0 \cf3  
-\f0\b \cf2 when
-\f1\b0 \cf3  \cf4 discounted_price_gbp\cf3  != \cf4 original_price_gbp\cf3  
-\f0\b \cf2 then
-\f1\b0 \cf0 \
-\cf3 		(\cf4 discounted_price_gbp\cf3  / \cf4 original_price_gbp\cf3 ) - \cf7 1\cf0 \
-\cf3 	
-\f0\b \cf2 end
-\f1\b0 \cf3  
-\f0\b \cf2 as
-\f1\b0 \cf3  \cf4 real_discount\cf3 ,\cf0 \
-\cf3 	\cf4 product_type\cf3 ,\cf0 \
-\cf3 	\cf4 sale_name\cf3 ,\cf0 \
-\cf3 	\cf4 rank_in_sale\cf3 ,\cf0 \
-\cf3 	\cf4 date_recorded\cf0 \
-\pard\pardeftab720\partightenfactor0
-
-\f0\b \cf2 from
-\f1\b0 \cf3  \cf4 base\cf0 \
-}
+select
+	product_name,
+	platform,
+	is_available,
+	original_price_gbp,
+	case when discounted_price_gbp != original_price_gbp then True else False end as is_discounted,
+	case when discounted_price_gbp != original_price_gbp then discounted_price_gbp else null end as discounted_price_gbp,
+	discount as display_discount,
+	case when discounted_price_gbp != original_price_gbp then
+		(discounted_price_gbp / original_price_gbp) - 1
+	end as real_discount,
+	product_type,
+	sale_name,
+	rank_in_sale,
+	date_recorded
+from base
