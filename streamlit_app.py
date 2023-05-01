12181d667dd5cc9e4afaddf4ace8e8cb612a72af
Cai Parry-Jones <97813242+caitpj@users.noreply.github.com>
2023-05-01 15:13:48 +0100

    add MySQL database functionality

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,13 @@
+# streamlit_app.py
 
+import streamlit as st
+
+# Initialize connection.
+conn = st.experimental_connection('mysql', type='sql')
+
+# Perform query.
+df = conn.query('SELECT * from clean_playstation_data;', ttl=600)
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
