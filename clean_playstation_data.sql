cc41f4e5e23936dda4c2686ac263219b8fc0da4b
Cai Parry-Jones <97813242+caitpj@users.noreply.github.com>
2023-05-01 14:57:01 +0100

    Account for trimming data

--- clean_playstation_data.sql
+++ clean_playstation_data.sql
@@ -1,34 +1,51 @@
 create or replace view clean_playstation_data as
 
-	with base as (
-		select 
-			name as product_name,
-			platforms as platform,
-			case when lower(original_price) = 'unavailable' or original_price is null then False else True end as is_available,
-			cast(CASE 
-				when lower(original_price) in ('free') or lower(original_price) like '%trial%' then 0
-				when lower(original_price) = 'unavailable' or original_price is null then null
-				when original_price like '£%' then REPLACE(original_price, '£', '')
-				else original_price
-			END as DECIMAL(10,2)) as original_price_gbp,
-			cast(CASE 
-				when lower(discount_price) in ('free') or lower(discount_price) like '%trial%' then 0
-				when lower(discount_price) = 'unavailable' or discount_price is null then null
-				when discount_price like '£%' then REPLACE(discount_price, '£', '')
-				else discount_price
-			END as DECIMAL(10,2)) as discounted_price_gbp,
-			discount,
-			product_type,
-			sale_name,
-			rank_in_sale,
-			`date` as date_recorded
-		FROM my_db.playstation_store_games
-	)
+with 
+trim as (
+	SELECT 
+		TRIM(BOTH "'" FROM name) AS name,
+		TRIM(BOTH "'" FROM platforms) AS platforms,
+		TRIM(BOTH "'" FROM original_price) AS original_price,
+		TRIM(BOTH "'" FROM discount_price) AS discount_price,
+		TRIM(BOTH "'" FROM discount) AS discount,
+		TRIM(BOTH "'" FROM product_type) AS product_type,
+		rank_in_sale,
+		TRIM(BOTH "'" FROM sale_name) AS sale_name,
+		STR_TO_DATE(TRIM(BOTH "'" FROM date), "%Y-%m-%d") AS date
+	FROM my_db.playstation_store_games
+),
 
+clean as (
+	select 
+		name as product_name,
+		platforms as platform,
+		case when lower(original_price) = 'unavailable' or original_price is null then False else True end as is_available,
+		cast(CASE 
+			when lower(original_price) in ('free') or lower(original_price) like '%trial%' then 0
+			when lower(original_price) = 'unavailable' or original_price is null then null
+			when original_price like '£%' then REPLACE(original_price, '£', '')
+			else original_price
+		END as DECIMAL(10,2)) as original_price_gbp,
+		cast(CASE 
+			when lower(discount_price) in ('free') or lower(discount_price) like '%trial%' then 0
+			when lower(discount_price) = 'unavailable' or discount_price is null then null
+			when discount_price like '£%' then REPLACE(discount_price, '£', '')
+			else discount_price
+		END as DECIMAL(10,2)) as discounted_price_gbp,
+		discount,
+		product_type,
+		sale_name,
+		rank_in_sale,
+		`date` as date_recorded
+	FROM trim
+),
+
+final as (
 	select
 		product_name,
 		platform,
 		is_available,
+		ifnull(discounted_price_gbp, original_price_gbp) as current_price,
 		original_price_gbp,
 		case when discounted_price_gbp != original_price_gbp then True else False end as is_discounted,
 		case when discounted_price_gbp != original_price_gbp then discounted_price_gbp else null end as discounted_price_gbp,
@@ -40,4 +57,8 @@ create or replace view clean_playstation_data as
 		sale_name,
 		rank_in_sale,
 		date_recorded
-	from base
+	from clean
+)
+
+select * from final
+;
